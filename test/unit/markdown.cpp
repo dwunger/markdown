d5962c29bb6b282f322e7f561191e52b6accf717
dwunger <106967706+dwunger@users.noreply.github.com>
2024-05-11 08:18:12 -0400

    Added test suites for line iterators

--- test/unit/markdown.cpp
+++ test/unit/markdown.cpp
@@ -1,10 +1,25 @@
 #include <gtest/gtest.h>
 
 extern "C" {
+#include <stdbool.h>
 #include "markdown.h"
 }
 
 TEST(MarkdownTest, ContainsInlineLink) {
     EXPECT_EQ(contains_inline_link("[Website Name](https://www.example.com/)"), 1);
     EXPECT_EQ(contains_inline_link("No inline link"), 0);
+}
+
+TEST(MarkdownTest, has_next_line) {
+    EXPECT_EQ(has_next_line("Line 1\nLine 2\nLine 3"), true);
+    EXPECT_EQ(has_next_line("Line 3"), false);
+}
+
+TEST(MarkdownTest, next_line) {
+    char *multiline_str = "Line 1\nLine 2\nLine 3";
+    // Do nothing
+    EXPECT_STREQ("Line 1\nLine 2\nLine 3", multiline_str);
+    // Advance to next line
+    next_line(&multiline_str);
+    EXPECT_STREQ("Line 2\nLine 3", multiline_str);
 }
