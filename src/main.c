91d7c7e22072a3e77f5d4049553e42d2ced6dc45
dwunger <106967706+dwunger@users.noreply.github.com>
2024-05-11 10:14:24 -0400

    iterate over lines and write to stdout

--- src/main.c
+++ src/main.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <stdint.h>
 
 #include "markdown.h"
+#include "file.h"
 
-
-size_t get_file_size(const FILE *stream);
+char *get_line(const char *text, size_t line_number);
 size_t count(const char *str, size_t str_len, char delimiter);
 
 int main(int argc, char *argv[])
 {
+    /* Parse input args */
     if (argc < 2) {
         fprintf(stderr, "User must supply path to markdown file\n");
         exit(1);
@@ -23,35 +26,25 @@ int main(int argc, char *argv[])
     size_t file_size = get_file_size(file_ptr);
     char *contents = malloc(sizeof(char) * file_size);
     fread(contents, sizeof(char), file_size, file_ptr);
+   
     
-    size_t line_count = count(contents, file_size, '\n');
-    for (size_t line_number = 0; line_number < line_count; line_number++)
+    /* Parse markdown file */
+    const char *line = contents;
+    size_t len = 0;
+    char *mutable_copy; 
+    while (has_next_line(line))
     {
         // Iterate over lines
+        len = next_line(&line);
+        mutable_copy = calloc(len + 1, sizeof(char));
+        strncpy(mutable_copy, line, len);
+        printf("%s\n",mutable_copy);
+
     }
+
     /* free file buffer */
     free(contents);
     return 0;
-}
-
-size_t get_file_size(const FILE *stream)
-{
-    // Save cursor position
-    long store_position = 0;
-    store_position = ftell(stream);
-    
-    // Move cursor to start of file
-    fseek(stream, 0, SEEK_SET);
-    size_t fstart = ftell(stream);
-
-    // Advance cursor to EOF
-    fseek(stream, 0, SEEK_END);
-    size_t fend = ftell(stream);
-    
-    // Restore cursor position
-    fseek(stream, store_position, SEEK_SET);
-
-    return (size_t)(fend - fstart);
 }
 
 size_t count(const char *str, size_t str_len, char delimiter) {
@@ -60,9 +53,7 @@ size_t count(const char *str, size_t str_len, char delimiter) {
     }
     size_t count = 0;
     for (size_t idx = 0; idx < str_len; idx++) {
-        if (str[idx] == delimiter) {
-            count++;
-        }
+        if (str[idx] == delimiter) { count++; }
     }
     return count;
 }
